c33da52ab3b2f0fbb91c388798514180140a62ef
Latasha Simmons <lsimmons1832@gmail.com>
2017-02-18 10:42:29 -0600

    added aug files

--- javascripts/solarsystem.js
+++ javascripts/solarsystem.js
@@ -2,7 +2,7 @@ var SolarSystem = (function(){
 	var planets = ["mercury","venus","earth","mars","jupiter","saturn","uranus","neptune"];
 	var dwarfPlanets = ["Pluto"];
 	var numPlanetsPeopleLandedOn = 0;
-	var spaceships = [];
+	
 	var stars = ["Sun","Alpha Centauri","Wolf 359"];
 	var age = 0;
 
@@ -16,15 +16,7 @@ var SolarSystem = (function(){
 		setPlanetsLandedPeopleOn: function(){
 			numPlanetsPeopleLandedOn++;
 		},
-		getSpaceShips: function(){
-			return spaceships;
-		},
-		setSpaceShips: function(newSpaceShip){
-			spaceships.push(newSpaceShip);
-		},
-		wreckedSpaceShips: function(){
-			spaceships.pop();
-		},
+
 		getDwarfPlanets: function(){
 			return "they are rejects";
 		},
@@ -47,10 +39,10 @@ var SolarSystem = (function(){
 
 })();
 
-console.log("Planets: ", SolarSystem.getPlanets());
-console.log("Planets with people: ", SolarSystem.getPlantesLandedPeopleOn());
-SolarSystem.setSpaceShips("Voyager 1");
-SolarSystem.setSpaceShips("Voyager 2");
-SolarSystem.setSpaceShips("Gemini");
-SolarSystem.setSpaceShips("Apollo");
-console.log("Where's my aircraft? ", SolarSystem.getSpaceShips());
+// console.log("Planets: ", SolarSystem.getPlanets());
+// console.log("Planets with people: ", SolarSystem.getPlantesLandedPeopleOn());
+// SolarSystem.setSpaceShips("Voyager 1");
+// SolarSystem.setSpaceShips("Voyager 2");
+// SolarSystem.setSpaceShips("Gemini");
+// SolarSystem.setSpaceShips("Apollo");
+// console.log("Where's my aircraft? ", SolarSystem.getSpaceShips());
